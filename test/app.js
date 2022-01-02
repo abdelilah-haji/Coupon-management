2177180b901ef78ebafe5583ceaab78771f2590c
Abdelilah Haji <abdelhajji6@gmail.com>
2022-01-02 02:33:59 +0100

    another commit

--- test/app.js
+++ test/app.js
@@ -15,18 +15,18 @@
 // 	t.end();
 // });
 
-// test('Correct coupons returned', function (t) {
-// 	var app = require('../app/app');
-// 	request(app)
-// 	.get('/api/coupons?password=' + process.env.API_PASSWORD)
-// 	.expect('Content-Type', /json/)
-// 	.expect(200)
-// 	.end(function (err, res) {
-// 		t.error(err, 'No error');
-// 		t.ok(res.body.length, 'Returned coupons list');
-// 		t.ok(res.body[0].code, 'Coupon #0 existed');
-// 		//t.same(res.body, expectedUsers, 'Users as expected');
-// 		t.end();
-// 		app.closeDatabase();
-// 	});
-// });
+test('Correct coupons returned', function (t) {
+	var app = require('../app/app');
+	request(app)
+	.get('/api/coupons?password=' + process.env.API_PASSWORD)
+	.expect('Content-Type', /json/)
+	.expect(200)
+	.end(function (err, res) {
+		t.error(err, 'No error');
+		t.ok(res.body.length, 'Returned coupons list');
+		t.ok(res.body[0].code, 'Coupon #0 existed');
+		//t.same(res.body, expectedUsers, 'Users as expected');
+		t.end();
+		app.closeDatabase();
+	});
+});
